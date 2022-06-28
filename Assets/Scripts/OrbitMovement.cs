60f1ba25208803a90bdc4b8dc9176bc66c292148
Caglan Atay <caglanatay@windowslive.com>
2022-06-28 17:33:56 +0300

    Script was created for orbital rotation.

---
+++ Assets/Scripts/OrbitMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _targetObject;
+    [SerializeField] private float _speedAmount;
+    private float _fullRotationAngle = 360f;
+    private IEnumerator _rotationRoutine;
+
+    private void Start()
+    {
+        StartOrbitalRotationRoutine();
+    }
+
+    private void OnDestroy() 
+    {
+        StopOrbitalRotationRoutine();
+    }
+
+    private void StartOrbitalRotationRoutine()
+    {
+        StopOrbitalRotationRoutine();
+
+        _rotationRoutine = OrbitalRotationProgress();
+
+        StartCoroutine(_rotationRoutine);
+    }
+
+    private void StopOrbitalRotationRoutine()
+    {
+        if(_rotationRoutine != null)
+        {
+            StopCoroutine(_rotationRoutine);
+        }
+        
+    }
+    
+    private IEnumerator OrbitalRotationProgress()
+    {
+        float totalAngle = 0;
+        while(true)
+        {
+            
+            transform.RotateAround(_targetObject.position, Vector3.up, _speedAmount * Time.deltaTime);
+            totalAngle += _speedAmount * Time.deltaTime;
+
+            if(totalAngle >= _fullRotationAngle)
+            {
+                WriteToConsolePlanetName(gameObject.name);
+                totalAngle = 0;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+    private void WriteToConsolePlanetName(string planetName)
+    {
+        Debug.Log($"{planetName} has completed its full rotation.");
+    }
+}
