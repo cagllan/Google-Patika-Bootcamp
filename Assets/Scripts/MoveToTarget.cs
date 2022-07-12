fd9a51712ca2af0a41eadfcb83df8145e9b0a475
Caglan Atay <caglanatay@windowslive.com>
2022-07-12 04:11:20 +0300

    Added meteor movement towards planet.

---
+++ Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour
+{
+    [SerializeField] private Transform _targetObject;
+    [SerializeField] private float _speedConf;
+
+
+    private Vector3 _distanceToTarget;
+    private Rigidbody _rigidbody;
+
+    private void Awake() 
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _targetObject = PlanetManager.Instance.GetRandomPlanet().transform;
+    }
+
+    private void Update() 
+    {
+        if(!_targetObject) return; 
+
+        _distanceToTarget = _targetObject.position - transform.position;
+        _distanceToTarget.Normalize();
+        _rigidbody.AddForce(_distanceToTarget * _speedConf);
+    }
+
+    private void OnDrawGizmos() 
+    {
+         Gizmos.color = Color.green;        
+         Gizmos.DrawLine(_targetObject.position, transform.position + _distanceToTarget);
+    }
+}
---
+++ Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    private static PlanetManager _instance;
+
+    public static PlanetManager Instance 
+    { 
+        get 
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<PlanetManager>();
+            }
+            return _instance;
+        }
+    }
+
+
+    private  Planet[] _planets;
+
+    private   Planet[] _Planets
+    { 
+        get 
+        {
+            if(_planets == null)
+            {
+                _planets = FindObjectsOfType<Planet>();
+            }
+            return _planets;
+        }
+    }
+
+
+    public Planet GetRandomPlanet()
+    {
+        return _Planets[Random.Range(0, _Planets.Length-1)];
+    } 
+}
